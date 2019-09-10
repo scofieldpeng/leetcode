19011d88f58018599247ca133d18faa08b72a6e9
scofieldpeng <penjianfeng@gmail.com>
2019-09-10 21:50:49 +0800

    update numSmallerByFrequency note

--- numSmallerByFrequency/numSmallerByFrequency.go
+++ numSmallerByFrequency/numSmallerByFrequency.go
@@ -76,3 +76,48 @@ func smallestCharacterNum(character string) int {
 
 	return res
 }
+
+// 这个是我看到非常精妙的答案,精妙的地方在于我上面的解法的时间复杂度是2n+n2,并且这里还是没有包括对words的结果集进行排序的时间复杂度
+// 而这里，他与我的恰恰相反，先求出words的counter结果集，然后此时通过一个循环，能够计算出对于任意一个范围内的数组，比他大的数字有多少个
+func numSmallerByFrequency2(queries []string, words []string) []int {
+
+	lookup := make([]int, 11)
+	for _, w := range words {
+		n := counter(w)
+		// 精妙的在于这里，对于每次求出的结果都放到lookup这个变量，这里最开始不太明白，明白了之后不得不鼓下掌，
+		// 举个例子就明白了，如果是对于1，2，3，4，5来说，如果是1，那么比1大于的值是4，对于2来说，值为3，因此，如果是连续的值，对于当前值
+		// 来说就是，最大的那个值减去当前值，那么，如果不是连续的呢？我们只需要知道，对于比他大的数字，出现过多少次就好了
+		// 比如我们这里只有1，2，3，5，那么对于5来说，对于1来说的话是大于的，对于2，3也是如此，因此就在这几个数字的头上增加1，这样后面我
+		// 们就能直接查看一个对应数字的大于的数量了，非常精妙，当然，这里的限制条件就是，只有最小值的最长的数量是固定的才可以，否则反而还会
+		// 增加查询次数
+		for j := n; j > 0; j-- {
+			// 这里为什么要j-1呢，是因为数组下标是从0开始的
+			lookup[j-1]++
+		}
+	}
+
+	result := make([]int, len(queries))
+	for i, q := range queries {
+		n := counter(q)
+		result[i] = lookup[n]
+	}
+	fmt.Println(lookup)
+	return result
+}
+
+// 他这里和我的解法也不同，他是生成了一个26位长度的数组，然后将对应字母出现的次数存储在对应下标，然后只需要获取到最小的字母即可
+// 当然，用数组的解法可以这样，但是我认为没必要，浪费了26个字节的数组空间
+func counter(s string) int {
+	si := 27
+	index := [26]int{}
+
+	for i:=0; i < len(s); i++ {
+		c := int(s[i] - 97)
+		if si > c {
+			si = c
+		}
+		index[c]++
+	}
+
+	return index[si]
+}
