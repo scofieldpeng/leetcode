b89b44c7e28a6f42d46dfc62d46fddf6337dfa69
scofieldpeng <penjianfeng@gmail.com>
2019-09-24 23:56:33 +0800

    update matrixReshape

---
+++ matrixReshape/matrixReshape_test.go
@@ -0,0 +1,15 @@
+package matrixReshape
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_matrixReshape(t *testing.T) {
+	nums := [][]int{{1,2},{3,4}}
+	r := 4
+	c := 1
+
+	res := matrixReshape(nums,r,c)
+	fmt.Println(res)
+}
