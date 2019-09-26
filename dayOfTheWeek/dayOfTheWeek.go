40b1ab077814982a1df29ebe47785553d1c435ec
scofieldpeng <penjianfeng@gmail.com>
2019-09-26 20:07:32 +0800

    update dayOfTheWeek

---
+++ dayOfTheWeek/dayOfTheWeek.go
@@ -0,0 +1,20 @@
+package dayOfTheWeek
+
+func dayOfTheWeek(day int, month int, year int) string {
+	week := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"}
+	var w int = 0
+
+	if month == 1 || month == 2{
+		month = 12 + month
+		year = year - 1
+	}
+
+	if (year < 1752) || (year == 1752 && month < 9) || (year == 1752 && month == 9 && day < 3) {
+		w = (day + 2*month + 3*(month+1)/5 + year + year/4 +5) % 7
+	} else {
+		w = (day + 2*month + 3*(month+1)/5 + year + year/4 -year/100 + year/400) % 7
+	}
+
+	return week[w]
+
+}
---
+++ dayOfTheWeek/dayOfTheWeek_test.go
@@ -0,0 +1,9 @@
+package dayOfTheWeek
+
+import "testing"
+
+func Test_dayOfTheWeek(t *testing.T) {
+	println(dayOfTheWeek(18,7,1999))
+	println(dayOfTheWeek(23,9,2019))
+	println(dayOfTheWeek(7,3,2003))
+}
