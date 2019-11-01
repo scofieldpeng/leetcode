8565e0521218d4d04a8eda75b50c952105627eba
scofieldpeng <penjianfeng@gmail.com>
2019-11-01 15:01:06 +0800

    update note

--- distanceBetweenBusStops/distanceBetweenBusStops.go
+++ distanceBetweenBusStops/distanceBetweenBusStops.go
@@ -4,30 +4,30 @@ package distanceBetweenBusStops
 
 func distanceBetweenBusStops(distance []int, start int, destination int) int {
 	var (
-		res1     = 0
-		res2     = 0
-		current  = 0
-		length   = len(distance)
+		res1    = 0
+		res2    = 0
+		current = 0
+		length  = len(distance)
 	)
 
 	if start == destination {
 		return 0
 	}
 
-	// 确保始终都是从左到右遍历
+	// 确保start比destination小
 	if start > destination {
 		start, destination = destination, start
 	}
-	current = start
 
 	// 正向遍历和反向遍历，然后看两个值那个值最少
 
+	current = start
 	for ; current != destination; current = (current + 1) % length {
 		res1 += distance[current]
 	}
 
 	current = destination
-	for ; current != start; current = (current + 1) % length{
+	for ; current != start; current = (current + 1) % length {
 		res2 += distance[current]
 	}
 
