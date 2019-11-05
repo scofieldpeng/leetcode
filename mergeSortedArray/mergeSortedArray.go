b9cb4709a0fc378befb2cf14c460609127328d2f
scofieldpeng <penjianfeng@gmail.com>
2019-11-05 15:10:18 +0800

    update

--- mergeSortedArray/mergeSortedArray.go
+++ mergeSortedArray/mergeSortedArray.go
@@ -3,18 +3,20 @@
 package mergeSortedArray
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	for ; m > 0 && n > 0; {
+	resLastIndex := m + n - 1
+	for ; m > 0 && n > 0; resLastIndex-- {
 		if nums1[m-1] >= nums2[n-1] {
-			nums1[m+n-1] = nums1[m-1]
+			nums1[resLastIndex] = nums1[m-1]
 			m -= 1
 			continue
 		}
 
-		nums1[m+n-1] = nums2[n-1]
+		nums1[resLastIndex] = nums2[n-1]
 		n -= 1
 	}
 
-	for ;n>0;n-- {
-		nums1[n-1] = nums2[n-1]
+	for ; n > 0; n-- {
+		nums1[resLastIndex] = nums2[n-1]
+		resLastIndex--
 	}
 }
