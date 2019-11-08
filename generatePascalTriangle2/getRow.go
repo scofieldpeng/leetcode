818095cf4007db2b23dc34bf562a4b510823a4ef
scofieldpeng <penjianfeng@gmail.com>
2019-11-08 14:25:20 +0800

    add pascal triangle 2 solution

---
+++ generatePascalTriangle2/getRow.go
@@ -0,0 +1,14 @@
+package generatePascalTriangle2
+
+func getRow(rowIndex int) []int {
+	var res = make([]int, rowIndex, rowIndex)
+	res[0] = 1
+	for i := 1; i < rowIndex; i++ {
+		// 第一位永远不会变,所有下标0永远不命中
+		for j := i; j >= 1; j-- {
+			res[j] = res[j] + res[j-1]
+		}
+	}
+
+	return res
+}
---
+++ generatePascalTriangle2/getRow_test.go
@@ -0,0 +1,35 @@
+package generatePascalTriangle2
+
+import (
+	"testing"
+)
+
+func Test_getRow(t *testing.T) {
+	inputs := []int{1, 2, 3, 4,}
+	outputs := [][]int{
+		{1},
+		{1, 1},
+		{1, 2, 1},
+		{1, 3, 3, 1},
+	}
+	for k, v := range inputs {
+		output := getRow(v)
+		if false == equal(output, outputs[k]) {
+			t.Fatalf("input: %v, wanted: %v, get: %v\n", v, outputs[k], output)
+		}
+	}
+}
+
+func equal(data1, data2 []int) bool {
+	if len(data1) != len(data2) {
+		return false
+	}
+
+	for k, v := range data1 {
+		if v != data2[k] {
+			return false
+		}
+	}
+
+	return true
+}
