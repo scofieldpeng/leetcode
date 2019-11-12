c932715a32eda69698b80bf8ddf5dd45f345a530
scofieldpeng <penjianfeng@gmail.com>
2019-11-12 14:02:34 +0800

    finish best time to buy and sell stock ii

--- bestTimeToBuyAndSellStockII/maxProfit.go
+++ bestTimeToBuyAndSellStockII/maxProfit.go
@@ -1,14 +1,19 @@
+// problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+// solution: https://algs.home.pjf.im/best-time-to-buy-and-sell-stock-ii.html
 package bestTimeToBuyAndSellStockII
 
 func maxProfit(prices []int) int {
 	var (
 		currentProfit = 0
-		maxProfit     = 0
 		length        = len(prices)
 	)
 
 	for i := 1; i < length; i++ {
+		// 如果涨价继续持有
+		if prices[i]-prices[i-1] > 0 {
+			currentProfit += prices[i] - prices[i-1]
+		}
 	}
 
-	return maxProfit
+	return currentProfit
 }
