2360d59f30f31a3d10adea0b8b11ae96852c5113
scofieldpeng <penjianfeng@gmail.com>
2019-11-16 00:22:21 +0800

    add majority element problem solution of boyer moore voting algs

--- majorityElement/majorityElement.go
+++ majorityElement/majorityElement.go
@@ -37,27 +37,30 @@ func majorElementByDivideAndConquer(nums []int) int {
 }
 
 func majorElementRec(nums []int, low, high int) int {
+	fmt.Printf("遍历元素开始,开始:%d,结束:%d\n", low, high)
 	if low == high {
-		fmt.Printf("marjorElementRec low equal high,return, low: %d, high: %d \n", low, high)
+		fmt.Printf("遍历一个元素，直接返回，遍历的元素是nums[%d]=%d\n", low, nums[low])
 		return nums[low]
 	}
 
 	mid := (high-low)/2 + low
-	fmt.Printf("marjorElementrec low: %d, hight: %d, mid: %d \n", low, high, mid)
+	fmt.Printf("计算本轮的开始%d,中间:%d,结束:%d\n", low, mid, high)
 	left := majorElementRec(nums, low, mid)
+	fmt.Printf("本轮计算后开始(%d)和中间(%d)的胜出结果是%d\n", low, mid, left)
 	right := majorElementRec(nums, mid+1, high)
+	fmt.Printf("本轮计算后中间+1(%d)和结尾(%d)的胜出结果是%d\n", mid+1, high, right)
 
 	if left == right {
-		fmt.Printf("majorElementRec left==right, left: %d, right: %d \n", left, right)
-		return nums[left]
+		fmt.Printf("左右两边(%d:%d)的结果一致,返回结果(开始:%d,结束:%d)\n", left, right, low, high)
+		return left
 	}
 
 	leftCount := elementCount(nums, left, low, high)
-	fmt.Printf("majorElementRec call elementCount left:%d, low:%d, high:%d,leftCount:%d\n", left, low, high, leftCount)
 	rightCount := elementCount(nums, right, low, high)
-	fmt.Printf("majorElementRec call elementCount right:%d, low:%d, high:%d,leftCount:%d\n", right, low, high, rightCount)
 
-	if leftCount > rightCount {
+	fmt.Printf("计算左边的最大值(%d)和右边的最大值(%d)在nums[%d]-nums[%d]中的出现次数%d:%d\n", left, right, low, high, leftCount, rightCount)
+
+	if leftCount >= rightCount {
 		return left
 	}
 
@@ -74,4 +77,24 @@ func elementCount(nums []int, number int, start, end int) int {
 	}
 
 	return count
+}
+
+// BoyerMoore vote solution
+// see: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+// or can see: http://www.cs.utexas.edu/~moore/best-ideas/mjrty/example.html
+func majorElementByBoyerMooreVoteAlgs(nums []int) int {
+	num := 0
+	count := 0
+	for _, v := range nums {
+		if count == 0 {
+			num = v
+		}
+		if num == v {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return num
 }
--- majorityElement/majorityElement_test.go
+++ majorityElement/majorityElement_test.go
@@ -21,7 +21,7 @@ func Test_majorityElement(t *testing.T) {
 
 func Test_majorElementByDivideAndConquer(t *testing.T) {
 	inputs := [][]int{
-		{3, 2, 3},
+		{2, 3, 3},
 	}
 	outputs := []int{
 		3,
@@ -29,6 +29,23 @@ func Test_majorElementByDivideAndConquer(t *testing.T) {
 
 	for k, v := range inputs {
 		res := majorElementByDivideAndConquer(v)
+		if res != outputs[k] {
+			t.Fatalf("input: %v, wanted: %v, get: %v\n", v, outputs[k], res)
+		}
+	}
+}
+
+func Test_majorElementByBoyerMooreVoteAlgs(t *testing.T) {
+	inputs := [][]int{
+		{3, 2, 3},
+		{2, 2, 1, 1, 1, 2, 2},
+	}
+	outputs := []int{
+		3, 2,
+	}
+
+	for k, v := range inputs {
+		res := majorElementByBoyerMooreVoteAlgs(v)
 		if res != outputs[k] {
 			t.Fatalf("input: %v, wanted: %v, get: %v\n", v, outputs[k], res)
 		}
