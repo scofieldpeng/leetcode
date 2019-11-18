474f15ebd63ddbf7c92abb3a2947df9133c01943
scofieldpeng <penjianfeng@gmail.com>
2019-11-18 14:37:24 +0800

    add remark

---
+++ containsDuplicateII/containsNearbyDuplicate.go
@@ -0,0 +1,11 @@
+// problem: https://leetcode.com/problems/contains-duplicate-ii
+// solution: https://algs.home.pjf.im/contains-duplicate-ii.html
+package containsDuplicateII
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	if len(nums) <= 2 {
+		return false
+	}
+
+	return false
+}
