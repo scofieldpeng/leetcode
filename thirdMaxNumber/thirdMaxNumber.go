1e1479ffcc684095a11a03d50777c68134b9d10a
scofieldpeng <penjianfeng@gmail.com>
2019-11-28 14:11:18 +0800

    add thirdMaxNumber solution

--- thirdMaxNumber/thirdMaxNumber.go
+++ thirdMaxNumber/thirdMaxNumber.go
@@ -1,13 +1,38 @@
 package thirdMaxNumber
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func thirdMax(nums []int) int {
 	sort.Ints(nums)
-
-	if len(nums) > 2 {
-		return nums[len(nums)-3]
+	data := make([]int, 3, 3)
+	data[0] = math.MinInt64
+	data[1] = math.MinInt64
+	data[2] = math.MinInt64
+	for _, v := range nums {
+		calculateThird(data, v)
 	}
 
-	return nums[len(nums)-1]
+	if data[2] == math.MinInt64 {
+		return data[0]
+	}
+
+	return data[2]
+}
+
+func calculateThird(nums []int, data int) {
+	if data == nums[0] || data == nums[1] || data == nums[2] {
+		return
+	}
+	if data > nums[2] {
+		nums[2], data = data, nums[2]
+		if nums[2] > nums[1] {
+			nums[1], nums[2] = nums[2], nums[1]
+			if nums[1] > nums[0] {
+				nums[0], nums[1] = nums[1], nums[0]
+			}
+		}
+	}
 }
