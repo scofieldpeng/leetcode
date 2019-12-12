390bd8488c3c3cbfa4b79cd183fe5e9680692b77
scofieldpeng <penjianfeng@gmail.com>
2019-12-12 15:06:54 +0800

    add stack solution of unsortedSubarray

--- findUnsortedSubarray/findUnsortedSubarray.go
+++ findUnsortedSubarray/findUnsortedSubarray.go
@@ -29,3 +29,100 @@ func findUnsortedSubarray(nums []int) int {
 
 	return end - begin + 1
 }
+
+// stack法
+func findUnsortedSubarray2(nums []int) int {
+	var stack = NewStack()
+	var (
+		leftIndex  = len(nums) - 1
+		rightIndex = 0
+	)
+	// 找出最左侧那个下降的波谷
+	for k, v := range nums {
+		for ; stack.Length() > 0 && nums[stack.Peek()] > v; {
+			leftIndex = min(stack.Pop(), leftIndex)
+		}
+		stack.Push(k)
+	}
+
+	stack.Clear()
+
+	// 找出最右侧的那个上升的波谷
+	for i := len(nums) - 1; i >= 0; i-- {
+		for ; stack.Length() > 0 && nums[stack.Peek()] < nums[i]; {
+			rightIndex = max(stack.Pop(), rightIndex)
+		}
+		stack.Push(i)
+	}
+
+	if leftIndex == rightIndex {
+		return 0
+	}
+
+	return rightIndex - leftIndex + 1
+}
+
+type Stack struct {
+	length int
+	data   []int
+}
+
+func NewStack() *Stack {
+	return &Stack{length: 0, data: make([]int, 0),}
+}
+
+func (s *Stack) Length() int {
+	return s.length
+}
+
+func (s *Stack) Peek() int {
+	if s.length > 0 {
+		return s.data[s.length-1]
+	}
+
+	return 0
+}
+
+func (s *Stack) Valley() int {
+	if s.length > 0 {
+		return s.data[0]
+	}
+
+	return 0
+}
+
+func (s *Stack) Pop() int {
+	if s.length < 1 {
+		return 0
+	}
+	var last = s.Peek()
+	s.data = s.data[0 : s.length-1]
+	s.length--
+
+	return last
+}
+
+func (s *Stack) Push(data int) {
+	s.length++
+	s.data = append(s.data, data)
+}
+
+func (s *Stack) Clear() {
+	s.length = 0
+	s.data = make([]int, 0)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+
+	return a
+}
--- findUnsortedSubarray/findUnsortedSubarray_test.go
+++ findUnsortedSubarray/findUnsortedSubarray_test.go
@@ -17,3 +17,19 @@ func Test_findUnsortedSubArray(t *testing.T) {
 		}
 	}
 }
+
+func Test_findUnsortedSubArray2(t *testing.T) {
+	inputs := [][]int{
+		{2, 6, 4, 8, 10, 9, 15},
+	}
+	outputs := []int{
+		5,
+	}
+
+	for k, v := range inputs {
+		res := findUnsortedSubarray2(v)
+		if res != outputs[k] {
+			t.Fatalf("input:%v,wanted:%v,get:%v\n", v, outputs[k], res)
+		}
+	}
+}
