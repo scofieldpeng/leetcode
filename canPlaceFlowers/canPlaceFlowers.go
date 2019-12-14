8bddab271320126574afbf4ebf58d74c0bb6ed38
scofieldpeng <penjianfeng@gmail.com>
2019-12-14 14:03:14 +0800

    add second solution of canPlaceFlowers

--- canPlaceFlowers/canPlaceFlowers.go
+++ canPlaceFlowers/canPlaceFlowers.go
@@ -19,3 +19,25 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
 
 	return false
 }
+
+// 第一种算法的改良版
+// 1. 找到够n的就停止，不用继续了
+// 2. 如果i上能种树，那么i+1是不能种的，跳过
+func canPlaceFlowers2(flowerbed []int, n int) bool {
+	var canPlantNum = 0
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+			// 种上一朵小花
+			flowerbed[i] = 1
+			// 可以栽的花多一棵
+			canPlantNum++
+			// 这里可以加速一下，因为下一位肯定不能栽了
+			i++
+		}
+		if canPlantNum >= n {
+			return true
+		}
+	}
+
+	return false
+}
--- canPlaceFlowers/canPlaceFlowers_test.go
+++ canPlaceFlowers/canPlaceFlowers_test.go
@@ -22,3 +22,24 @@ func Test_canPlaceFlowers(t *testing.T) {
 		}
 	}
 }
+
+func Test_canPlaceFlowers2(t *testing.T) {
+	input1 := [][]int{
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+	}
+	input2 := []int{
+		1, 2,
+	}
+
+	outputs := []bool{
+		true, false,
+	}
+
+	for k, v := range input1 {
+		res := canPlaceFlowers2(v, input2[k])
+		if res != outputs[k] {
+			t.Fatalf("input: %v,%v, wanted: %v, get: %v\n", v, input2[k], outputs[k], res)
+		}
+	}
+}
