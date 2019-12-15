9fe3dc66f900cdc37d4d91a644f98867c3d39d96
scofieldpeng <penjianfeng@gmail.com>
2019-12-15 15:12:01 +0800

    fix maximumProduct solution

--- maximumProduct/maximumProduct.go
+++ maximumProduct/maximumProduct.go
@@ -16,5 +16,13 @@ func maximumProduct(nums []int) int {
 
 	length := len(nums)
 
-	return nums[length-1] * nums[length-2] * nums[length-3]
+	// 要不是最后一个乘以第一二个值，要不就是倒数3个值
+	lastProduct := nums[length-1] * nums[length-2] * nums[length-3]
+	negativeProduct := nums[length-1] * nums[0] * nums[1]
+
+	if lastProduct > negativeProduct {
+		return lastProduct
+	}
+
+	return negativeProduct
 }
