18b08e01d09740d413b427d1d8ce47e77c2c5baf
scofieldpeng <penjianfeng@gmail.com>
2019-12-17 13:59:14 +0800

    add second solution of findMaxAverage

--- findMaxAverage/findMaxAverage.go
+++ findMaxAverage/findMaxAverage.go
@@ -16,7 +16,9 @@ func findMaxAverage(nums []int, k int) float64 {
 	// 找到k个数组中最大的那个值，那么平均值也是最大的
 	// 怎么知道a到b的总和呢？
 	// 用b到0的总和减去a到0的总和即可
-	maxValue := 0
+
+	// 注意这里的k-1,让其默认值为第一个最大，避免k<len(nums)时的case
+	maxValue := numSumArr[k-1]
 	for i := k; i < length; i++ {
 		v := numSumArr[i] - numSumArr[i-k]
 		if maxValue < v {
@@ -25,4 +27,23 @@ func findMaxAverage(nums []int, k int) float64 {
 	}
 
 	return float64(maxValue) / float64(k)
+}
+
+func findMaxAverage2(nums []int, k int) float64 {
+	sumValue := 0
+	for i := 0; i < k; i++ {
+		sumValue += nums[i]
+	}
+
+	// 这里sumValue默认赋值给maxValue是防止k<len(nums)时的边界条件
+	maxValue := sumValue
+	for i := k; i < len(nums); i++ {
+		sumValue = sumValue + nums[i] - nums[i-k]
+		if sumValue > maxValue {
+			maxValue = sumValue
+		}
+	}
+
+	return float64(maxValue) / float64(k)
+
 }
--- findMaxAverage/findMaxAverage_test.go
+++ findMaxAverage/findMaxAverage_test.go
@@ -5,16 +5,40 @@ import "testing"
 func Test_findMaxAverage(t *testing.T) {
 	inputs1 := [][]int{
 		{1, 12, -5, -6, 50, 3},
+		{5},
 	}
 	inputs2 := []int{
 		4,
+		1,
 	}
 	outputs := []float64{
 		12.75,
+		5,
 	}
 
 	for k, v := range inputs1 {
 		res := findMaxAverage(v, inputs2[k])
+		if res != outputs[k] {
+			t.Fatalf("input: %v, %v, wanted: %v, get: %v\n", v, inputs2[k], outputs[k], res)
+		}
+	}
+}
+
+func Test_findMaxAverage2(t *testing.T) {
+	inputs1 := [][]int{
+		{1, 12, -5, -6, 50, 3},
+		{5},
+	}
+	inputs2 := []int{
+		4, 1,
+	}
+	outputs := []float64{
+		12.75,
+		5,
+	}
+
+	for k, v := range inputs1 {
+		res := findMaxAverage2(v, inputs2[k])
 		if res != outputs[k] {
 			t.Fatalf("input: %v, %v, wanted: %v, get: %v\n", v, inputs2[k], outputs[k], res)
 		}
