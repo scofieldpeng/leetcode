41c695180ea8f3d614c4481e1d9d5b8aef16be90
scofieldpeng <penjianfeng@gmail.com>
2019-12-28 14:24:32 +0800

    add solution of findLengthOfLCIS

--- findLengthOfLCIS/findLengthOfLCIS.go
+++ findLengthOfLCIS/findLengthOfLCIS.go
@@ -3,5 +3,28 @@
 package findLengthOfLCIS
 
 func findLengthOfLCIS(nums []int) int {
+	length := len(nums)
+	if length < 1 {
+		return 0
+	}
 
+	longest := 1
+	current := 1
+
+	for i := 1; i < length; i++ {
+		// 跳过了
+		if nums[i-1] >= nums[i] {
+			if longest < current {
+				longest = current
+			}
+			current = 1
+			continue
+		}
+		current++
+		if current > longest {
+			longest = current
+		}
+	}
+
+	return longest
 }
---
+++ findLengthOfLCIS/findLengthOfLCIS_test.go
@@ -0,0 +1,20 @@
+package findLengthOfLCIS
+
+import "testing"
+
+func Test_findLengthOfLCIS(t *testing.T) {
+	inputs := [][]int{
+		{1, 2, 5, 4, 7},
+		{2, 2, 2, 2, 2},
+	}
+	outouts := []int{
+		3, 1,
+	}
+
+	for k, v := range inputs {
+		res := findLengthOfLCIS(v)
+		if res != outouts[k] {
+			t.Fatalf("input:%v, wanted:%v, get:%v\n", v, outouts[k], res)
+		}
+	}
+}
