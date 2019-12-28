0461e022e07b1aaa7ffef7d3aeb42ea78787df4e
scofieldpeng <penjianfeng@gmail.com>
2019-12-28 14:12:14 +0800

    ready to solute findLengthOfLCIS

---
+++ findLengthOfLCIS/findLengthOfLCIS.go
@@ -0,0 +1,7 @@
+// problem: https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+// solution: https://algs.home.pjf.im/longest-continuous-increasing-subsequence.html
+package findLengthOfLCIS
+
+func findLengthOfLCIS(nums []int) int {
+
+}
