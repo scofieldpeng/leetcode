81bf64d26f2550aa6b28cce1c7beacc7b8927b9c
scofieldpeng <penjianfeng@gmail.com>
2019-12-30 23:25:49 +0800

    add third solution of isOneBitCharacter

--- isOneBitCharacter/isOneBitCharacter.go
+++ isOneBitCharacter/isOneBitCharacter.go
@@ -51,6 +51,24 @@ func isOneBitCharacter2(bits []int) bool {
 	return false
 }
 
+// 贪婪算法，虽然我看懂了solution的算法，但实际上我没有搞懂这跟贪婪算法有什么关系
+//
+// 我们仔细思考规则可以发现，如果一个字符是1，那么下一位不管是1还是0，下一位都是一个双位字符，也就是说，11，111x都是一个双位
+// 那么，我们就从右往左找最近的那个0（不包含最后一位数的那个0），只要找到了，然后看在这个0和最后一位之间间隔的1是多少位，如果是一个奇数位的
+// 话，那么就说明，最后一位的0肯定是这个1的组成部分，如果是偶数位，那么说明，这之间的1肯定都已经互相组合了
 func isOneBitCharacter3(bits []int) bool {
+	findOneCount := 0
+	for start := len(bits) - 2; start >= 0; start-- {
+		// 找1的数量，这里还可以用位运算, 但是对于go来说有点多此一举了
+		// bits[start] ^ 1 == 0
+		if bits[start] == 1 {
+			findOneCount++
+			continue
+		}
+		// 一旦找到0就break
+		break
+	}
 
+	// 如果找到的1是偶数，那么最后一位肯定是一个独位0
+	return findOneCount%2 == 0
 }
--- isOneBitCharacter/isOneBitCharacter_test.go
+++ isOneBitCharacter/isOneBitCharacter_test.go
@@ -35,3 +35,20 @@ func Test_isOneBitCharacter2(t *testing.T) {
 		}
 	}
 }
+
+func Test_isOneBitCharacter3(t *testing.T) {
+	inputs := [][]int{
+		{1, 0, 0},
+		{1, 1, 1, 0},
+	}
+	outputs := []bool{
+		true, false,
+	}
+
+	for k, v := range inputs {
+		res := isOneBitCharacter3(v)
+		if res != outputs[k] {
+			t.Fatalf("input: %v, wanted: %v, get: %v\n", v, outputs[k], res)
+		}
+	}
+}
