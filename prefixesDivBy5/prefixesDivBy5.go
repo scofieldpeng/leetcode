b67dfdb8b6313b89fc8bd6bb9f6914b281f18d90
scofieldpeng <penjianfeng@gmail.com>
2020-05-05 13:41:35 +0800

    finish #1018

--- prefixesDivBy5/prefixesDivBy5.go
+++ prefixesDivBy5/prefixesDivBy5.go
@@ -1,14 +1,16 @@
 package prefixesDivBy5
 
-import "math"
+import (
+	"strconv"
+)
 
 func prefixesDivBy5(A []int) []bool {
 	length := len(A)
 	res := make([]bool, length, length)
 	count := 0
 	for i, v := range A {
-		count += v * int(math.Mod(2, float64(i)))
-		if count%5 == 0 {
+		count = count * 10 + v
+		if k,_ := strconv.ParseInt(strconv.Itoa(count),2,64); k % 5== 0 {
 			res[i] = true
 		}
 	}
