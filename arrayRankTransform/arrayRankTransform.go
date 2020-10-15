e1d0e90c66004d683d9895b40379b883c5b23d88
scofieldpeng <penjianfeng@gmail.com>
2020-10-15 13:56:14 +0800

    better solution for problem 1331

--- arrayRankTransform/arrayRankTransform.go
+++ arrayRankTransform/arrayRankTransform.go
@@ -4,28 +4,24 @@ package arrayRankTransform
 import "sort"
 
 func arrayRankTransform(arr []int) []int {
-	maxScore := len(arr)
-	res := make([]int, maxScore, maxScore)
-	arrMap := make(map[int][]int)
+	arrLen := len(arr)
+	arrCopy := make([]int, arrLen, arrLen)
 	for k, v := range arr {
+		arrCopy[k] = v
+	}
+	sort.Ints(arrCopy)
+	arrMap := make(map[int]int)
+	start := 1
+	for _, v := range arrCopy {
 		if _, ok := arrMap[v]; !ok {
-			arrMap[v] = make([]int, 0)
+			arrMap[v] = start
+			start++
 		}
-		arrMap[v] = append(arrMap[v], k)
 	}
 
-	sort.Ints(arr)
-	rank := 0
-	for _, v := range arr {
-		// 从1开始
-		for kk, vv := range arrMap[v] {
-			if res[vv] == 0 {
-				if kk == 0 {
-					rank++
-				}
-				res[vv] = rank
-			}
-		}
+	res := make([]int, arrLen, arrLen)
+	for k, v := range arr {
+		res[k] = arrMap[v]
 	}
 
 	return res
